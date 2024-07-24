ec78f51ef3785cb57bb6d2e015f1c66835319c3e
EkaterinaFefelova <fefelova.ekaterina@gmail.com>
2024-07-24 11:06:36 +0300

    Task on arrays completed

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,33 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
-
+        int[] myArray = new int [10];
+        for (int i=0; i<myArray.length; i++)
+           myArray[i] = new Random().nextInt(75);
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
         //todo
+        System.out.println("Печатаю массив с начала");
+        for (int j : myArray) System.out.println(j);
 
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
+        System.out.println("\nПечатаю массив с конца");
+        for (int i = myArray.length-1; i>=0; i--)
+            System.out.println(myArray[i]);
 
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
+        int[] myArray1 = new int[myArray.length];
+        int j= myArray1.length-1;
+        for (int value : myArray) {
+            myArray1[j] = value;
+            j--;
+        }
+        System.out.println("\nПечатаю развернутый массив");
+        for (int k : myArray1)
+            System.out.println(k);
     }
 }
