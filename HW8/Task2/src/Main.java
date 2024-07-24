5e64b44e845ab863bcbd7acb4a256a754c1a6e00
EkaterinaFefelova <fefelova.ekaterina@gmail.com>
2024-07-24 11:41:46 +0300

    Random & average weight task completed

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //todo код программы писать тут
+        int[] weights = new int[10];
+        Random rand = new Random();
+        for (int i=0; i<weights.length; i++) weights[i] = rand.nextInt(100-40+1) + 40;
+        for (int value: weights) System.out.println(value);
+
+        int totalWeight = 0;
+        for (int value: weights) totalWeight += value;
+        int averageWeight = totalWeight/weights.length;
+        System.out.println("\nСредний вес: " + averageWeight);
+
+        int number = 0;
+        for (int value: weights) {
+            if (value>=60 && value<=80)
+                number++;
+        }
+        System.out.println("\nКоличество людей с весом в диапазоне от 60 до 80 кг: " + number);
     }
 }
